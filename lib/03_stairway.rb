774ad7c3ac5935ba8c16500a6d06be8aa116cb89
Noé Marguillard <noe.marguillard@essec.edu>
2019-01-10 23:02:20 +0100

    last

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,38 +1,68 @@
-def roll # la méthode permettant le jet de dés random
-  dice = rand (1..6) # un random entre 1 et 6
-  return dice
-  end
-  
-  def game(dice)
-      n = 0
-      x = 0
-    while n < 10 # tant que le personnage n'est pas arrivé en haut des 10 marches de l'escalier
-      dice = roll # on stock dans dice un nouveau lancé de dés random
-      if dice == 5 || dice == 6
-        n += 1
-        puts "Tu montes d'un étage ! Tu es sur la marche #{n}."
-        x = x+1 # compteur permettant de connaitre le nombre de lancés nécessaires pour monter les 10 marches de la pyramide
-      elsif dice == 1
-        n -= 1
-        puts "Tu descends d'un étage ! Tu es sur la marche #{n}."
-        x = x+1
-      else
-        n = n 
-        puts "Tu ne bouges pas ! Tu es sur la marche #{n}."
-        x = x+1
-      end
-    puts x # on affiche le nombre de lancés total pour chaque partie
-  end 
- 
+def game
+
+    p = 0
+    while p < 10
+
+        dice = rand (1..6)
+        puts "Ton lancer est un #{dice}."
+        
+        
+        if dice == 5 || dice == 6
+            p = p + 1
+            puts "Tu es sur la marche numéro #{p}."
+            puts""
+            i = 0
+            (9-p).times do
+                puts "#" * (i = i + 1)
+            end
+            puts "#"*(10-p) + "*"
+            i = 10-p
+            p.times do
+                puts "#" * (i = i + 1)
+            end
+            puts ""
+
+
+        elsif dice == 1
+            p = p - 1
+            puts "Tu es sur la marche numéro #{p}."
+            puts""
+            i = 0
+            (9-p).times do
+                puts "#" * (i = i + 1)
+            end
+            puts "#"*(10-p) + "*"
+            i = 10-p
+            p.times do
+                puts "#" * (i = i + 1)
+            end
+            puts ""
+
+
+        else
+            p
+            puts "Tu es sur la marche numéro #{p}."
+            puts""
+            i = 0
+            (9-p).times do
+                puts "#" * (i = i + 1)
+            end
+            puts "#"*(10-p) + "*"
+            i = 10-p
+            p.times do
+                puts "#" * (i = i + 1)
+            end
+            puts ""
+        end
+    end
     puts "Bravo tu as gagné!"
-      
-  end
+end
+
+def perform
+    game
+end
   
-  def perform
-    dice = roll
-    game(dice)
-  end
-  
-  100.times do 
+100.times do 
     puts perform
-  end
+end
+
